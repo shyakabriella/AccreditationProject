f267a75e2cbcbc66fe4a20164b6c37f649781033
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-06 18:50:08 +0200

    Modify logo in sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="side-nav side-nav-transparent side-nav-expand">
     <div class="side-nav-header">
-        <div class="px-6 logo">
-            <img src="img/logo/logo-light-full.png" alt="Elstar logo">
+        <div class="px-6 py-24 logo">
+            <h2 class="text-dark" style="font-size: 16px">ACCREDITATION</h2>
         </div>
     </div>
     <div class="relative side-nav-content side-nav-scroll">
@@ -10,9 +10,7 @@
                 <ul>
                     <li data-menu-item="dashboard" class="mb-2 menu-item menu-item-single">
                         <a class="menu-item-link" href="{{ url('dashboard') }}">
-                            <svg class="menu-item-icon" stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"></path>
-                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><path d="M2 12.204c0-2.289 0-3.433.52-4.381c.518-.949 1.467-1.537 3.364-2.715l2-1.241C9.889 2.622 10.892 2 12 2s2.11.622 4.116 1.867l2 1.241c1.897 1.178 2.846 1.766 3.365 2.715S22 9.915 22 12.203v1.522c0 3.9 0 5.851-1.172 7.063S17.771 22 14 22h-4c-3.771 0-5.657 0-6.828-1.212S2 17.626 2 13.725z"/><path stroke-linecap="round" d="M12 15v3"/></g></svg>
                             <span class="menu-item-text">Dashboard</span>
                         </a>
                     </li>
