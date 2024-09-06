9ec79479ee0be8e5c41c803232915623cd88e26a
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-06 18:59:51 +0200

    Implement logout functionality in header

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -21,8 +21,8 @@
                         <span class="avatar avatar-circle" data-avatar-size="32" style="width: 32px">
                         <img class="avatar-img avatar-circle" src="https://static.vecteezy.com/system/resources/thumbnails/005/129/844/small_2x/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg" loading="lazy" alt=""></span>
                         <div class="hidden md:block">
-                            <div class="text-xs capitalize">admin</div>
-                            <div class="font-bold">Carolyn Perkins</div>
+                            <div class="text-xs capitalize">{{ Auth::user()->role }}</div>
+                            <div class="font-bold">{{ Auth::user()->name }}</div>
                         </div>
                     </div>
                 </div>
@@ -39,12 +39,18 @@
                     </li>
                     <li id="menu-item-29-2VewETdxAb" class="menu-item-divider"></li>
                     <li class="menu-item menu-item-hoverable gap-2 h-[35px]">
-                        <span class="text-xl opacity-50">
-                            <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
-                            </svg>
-                        </span>
-                        <span>Sign Out</span>
+                        <a class="flex items-center gap-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <span class="text-xl opacity-50">
+                                <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
+                                </svg>
+                            </span>
+                            <span>Sign Out</span>
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" class="light">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<link rel="shortcut icon" href="img/favicon.ico">
-		<title>Accreditation</title>
-		<link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
-	</head>
-	<body>
-		<div id="root">
-			<div class="flex flex-col flex-auto app-layout-modern">
-				<div class="flex flex-auto min-w-0">
-					@include('inc.sidebar')
-					<div class="relative flex flex-col flex-auto w-full min-w-0 min-h-screen bg-white border-l border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <title>Accreditation</title>
+        <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
+    </head>
+    <body>
+        <div id="root">
+            <div class="flex flex-col flex-auto app-layout-modern">
+                <div class="flex flex-auto min-w-0">
+                    @include('inc.sidebar')
+                    <div class="relative flex flex-col flex-auto w-full min-w-0 min-h-screen bg-white border-l border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                         @include('inc.header')
-						<div class="flex flex-col justify-between flex-auto h-full">
-							<main class="h-full">
+                        <div class="flex flex-col justify-between flex-auto h-full">
+                            <main class="h-full">
                                 <div class="relative flex flex-col flex-auto h-full px-4 py-4 page-container sm:px-6 md:px-8 sm:py-6">
-								    @yield('content')
+                                    @yield('content')
                                 </div>
-							</main>
-							@include('inc.footer')
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script src="{{ url('js/vendors.min.js') }}"></script>
-		<script src="{{ url('js/app.min.js') }}"></script>
-	</body>
+                            </main>
+                            @include('inc.footer')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="{{ url('js/vendors.min.js') }}"></script>
+        <script src="{{ url('js/app.min.js') }}"></script>
+    </body>
 
 </html>
