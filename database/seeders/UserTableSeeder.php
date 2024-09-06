4d01ef0c820ff32bb95fdbb3701254ebde4bddc8
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-06 17:58:30 +0200

    Seed users

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role' => 'Admin',
+            'password' => bcrypt('admin@admin.com'),
+        ]);
+
+        User::create([
+            'name' => 'Institution',
+            'email' => 'institution@institution.com',
+            'role' => 'Institution',
+            'password' => bcrypt('institution@institution.com'),
+        ]);
+
+        User::create([
+            'name' => 'Company',
+            'email' => 'company@company.com',
+            'role' => 'Company',
+            'password' => bcrypt('company@company.com'),
+        ]);
+    }
+}
