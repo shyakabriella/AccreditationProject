8b9ec815b4a0821a8a8f8adb8459e860b2a54b69
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-09 18:40:02 +0200

    Create institution migration

--- database/migrations/2024_09_09_163828_create_institutions_table.php
+++ database/migrations/2024_09_09_163828_create_institutions_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('institutions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type')->nullable();
+            $table->string('physical_address')->nullable();
+            $table->string('province')->nullable();
+            $table->string('district')->nullable();
+            $table->string('sector')->nullable();
+            $table->string('cell')->nullable();
+            $table->string('village')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('owner_name')->nullable();
+            $table->string('owner_phone')->nullable();
+            $table->string('owner_email')->nullable();
+            $table->string('contact_person_name')->nullable();
+            $table->string('contact_person_phone')->nullable();
+            $table->string('contact_person_email')->nullable();
+            $table->date('registration_date')->nullable();
+            $table->string('registration_number')->nullable();
+            $table->string('legal_status')->nullable();
+            $table->text('mission')->nullable();
+            $table->text('objectives')->nullable();
             $table->timestamps();
         });
     }
