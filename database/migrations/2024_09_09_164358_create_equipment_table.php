c3a17521fa2f08ff8176ea7a340a94da0a31a5bc
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-09 18:45:00 +0200

    Add equipment migration

--- database/migrations/2024_09_09_164358_create_equipment_table.php
+++ database/migrations/2024_09_09_164358_create_equipment_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateEquipmentTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('equipment', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('institution_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('quantity')->nullable();
             $table->timestamps();
+
+            $table->foreign('institution_id')->references('id')->on('institutions')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('equipment');
     }
-};
+}
