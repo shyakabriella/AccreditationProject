5c8bee0f80a1f7c520290ec8324036c940235ca1
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-09 18:39:23 +0200

    Creae institution model

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -2,10 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Institution extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'type',
+        'physical_address',
+        'province',
+        'district',
+        'sector',
+        'cell',
+        'village',
+        'phone',
+        'email',
+        'owner_name',
+        'owner_phone',
+        'owner_email',
+        'contact_person_name',
+        'contact_person_phone',
+        'contact_person_email',
+        'registration_date',
+        'registration_number',
+        'legal_status',
+        'mission',
+        'objectives',
+    ];
+
+    public function trainingPrograms()
+    {
+        return $this->hasMany(TrainingProgram::class);
+    }
+
+    public function equipment()
+    {
+        return $this->hasMany(Equipment::class);
+    }
+
+    public function staff()
+    {
+        return $this->hasMany(Staff::class);
+    }
 }
