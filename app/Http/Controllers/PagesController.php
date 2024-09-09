a7530bd98b791bc7b1773726bd7004d54ac056cf
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-09 16:09:51 +0200

    Initialize frontend side

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+    public function home()
+    {
+        return view('frontend.index');
+    }
+
     public function dashboard()
     {
         return view('backend.dashboard');
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PagesController::class, 'home'])->name('home');
 
 Route::middleware(['auth', 'role:admin,institution,company'])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\PagesController::class, 'dashboard'])->name('dashboard');
