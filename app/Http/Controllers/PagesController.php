19cddb80646f5e33f4982cbe7570ae073e700ad0
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-09 17:34:27 +0200

    Initialize trade details page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -16,6 +16,11 @@ class PagesController extends Controller
         return view('frontend.trades.index');
     }
 
+    public function getTradeDetails()
+    {
+        return view('frontend.trades.show');
+    }
+
     public function dashboard()
     {
         return view('backend.dashboard');
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\PagesController::class, 'home'])->name('home');
 Route::get('/trades', [App\Http\Controllers\PagesController::class, 'getTrades'])->name('getTrades');
+Route::get('/trade/show', [App\Http\Controllers\PagesController::class, 'getTradeDetails'])->name('getTradeDetails');
 
 Route::middleware(['auth', 'role:admin,institution,company'])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\PagesController::class, 'dashboard'])->name('dashboard');
