6b134986f456e3caaeb39cdbb39e02fc103e4dbe
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-10 15:27:27 +0200

    Add routes for sending an updating request

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::middleware(['auth', 'role:admin,institution,company'])->group(function ()
     Route::put('/training-programs/{id}', [App\Http\Controllers\Admin\PagesController::class, 'updateTrainingProgram'])->name('updateTrainingProgram');
     Route::delete('/training-programs/{id}', [App\Http\Controllers\Admin\PagesController::class, 'deleteTrainingProgram'])->name('deleteTrainingProgram');
 
+    Route::post('/training-programs/{id}/send-application', [App\Http\Controllers\Admin\PagesController::class, 'sendApplication'])->name('sendApplication');
+    Route::post('/training-programs/{id}/approve', [App\Http\Controllers\Admin\PagesController::class, 'approveProgram'])->name('approveProgram');
+    Route::post('/training-programs/{id}/reject', [App\Http\Controllers\Admin\PagesController::class, 'rejectProgram'])->name('rejectProgram');
+
     Route::get('/applications', [App\Http\Controllers\Admin\PagesController::class, 'getApplications'])->name('getApplications');
     Route::post('/update-application-status', [App\Http\Controllers\Admin\PagesController::class, 'updateApplicationStatus'])->name('updateApplicationStatus');
 
