c1895b9d08d6cd0764ec3efe7703343dc8c9ad77
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-15 15:55:21 +0200

    Design contact us page

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -23,4 +23,51 @@
         </div>
     </div>
 
+    <section class="relative py-16 lg:py-24">
+        <div class="container">
+            <div class="grid md:grid-cols-12 grid-cols-1 items-center gap-[30px]">
+                <div class="lg:col-span-7 md:col-span-6">
+                    <img src="https://shreethemes.in/jobstack/layouts/assets/images/svg/contact.svg" alt="">
+                </div>
+
+                <div class="lg:col-span-5 md:col-span-6">
+                    <div class="lg:ms-5">
+                        <div class="p-6 bg-white rounded-md shadow dark:bg-slate-900 dark:shadow-gray-700">
+                            <h3 class="mb-6 text-2xl font-semibold leading-normal">Get in touch !</h3>
+
+                            <form method="post" name="myForm" id="myForm" onsubmit="return validateForm()">
+                                <p class="mb-0" id="error-msg"></p>
+                                <div id="simple-msg"></div>
+                                <div class="grid lg:grid-cols-12 lg:gap-6">
+                                    <div class="mb-5 lg:col-span-6">
+                                        <label for="name" class="font-semibold">Your Name:</label>
+                                        <input name="name" id="name" type="text" class="mt-2 border form-input border-slate-100 dark:border-slate-800" placeholder="Name :">
+                                    </div>
+
+                                    <div class="mb-5 lg:col-span-6">
+                                        <label for="email" class="font-semibold">Your Email:</label>
+                                        <input name="email" id="email" type="email" class="mt-2 border form-input border-slate-100 dark:border-slate-800" placeholder="Email :">
+                                    </div>
+                                </div>
+
+                                <div class="grid grid-cols-1">
+                                    <div class="mb-5">
+                                        <label for="subject" class="font-semibold">Your Question:</label>
+                                        <input name="subject" id="subject" class="mt-2 border form-input border-slate-100 dark:border-slate-800" placeholder="Subject :">
+                                    </div>
+
+                                    <div class="mb-5">
+                                        <label for="comments" class="font-semibold">Your Comment:</label>
+                                        <textarea name="comments" id="comments" class="mt-2 border form-input border-slate-100 dark:border-slate-800 textarea" placeholder="Message :"></textarea>
+                                    </div>
+                                </div>
+                                <button type="submit" id="submit" name="send" class="text-white rounded-md btn bg-emerald-600 hover:bg-emerald-700">Send Message</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
