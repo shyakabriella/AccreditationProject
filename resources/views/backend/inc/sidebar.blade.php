e90c62bc1e2e794546814e32d104c6ebb795d938
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-15 17:43:51 +0200

    Modify the sidebar

--- resources/views/backend/inc/sidebar.blade.php
+++ resources/views/backend/inc/sidebar.blade.php
@@ -1,13 +1,13 @@
 <div class="side-nav side-nav-transparent side-nav-expand">
     <div class="side-nav-header">
-        <div class="px-6 py-24 logo">
-            <h2 class="text-dark" style="font-size: 16px">SCAW</h2>
+        <div class="px-6 py-24">
+            <h2 class="text-center text-dark" style="font-size: 16px; padding-top: 10px">SCAW</h2>
         </div>
     </div>
     <div class="relative side-nav-content side-nav-scroll">
         <nav class="px-4 pb-4 menu menu-transparent">
             <div class="menu-group">
-                <ul>
+                <ul style="padding-top: 50px">
                     @if(auth()->user()->role === 'admin')
                         <li data-menu-item="dashboard" class="mb-2 menu-item menu-item-single">
                             <a class="menu-item-link" href="{{ route('dashboard') }}">
